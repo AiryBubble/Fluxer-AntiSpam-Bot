c84ef79ac3b45aa1e4f1327a6e8358f8b62935fd
AiryBubble <111546646+AiryBubble@users.noreply.github.com>
2026-08-05 11:28:31 +0900

    絵文字判定を緩和しました
    漢字なども正規表現に含まれていたため修正しました

--- main.py
+++ main.py
@@ -704,9 +704,16 @@ def check_flood(message):
 
 def check_emoji_spam(message):
     custom_emoji = len(re.findall(r'<a?:[a-zA-Z0-9_]+:[0-9]+>', message.content))
-    unicode_emoji = len(re.findall(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF'
-                                   r'\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF'
-                                   r'\U00002702-\U000027B0\U000024C2-\U0001F251]', message.content))
+    unicode_emoji = len(re.findall(
+      r'[\U0001F600-\U0001F64F'
+      r'\U0001F300-\U0001F5FF'
+      r'\U0001F680-\U0001F6FF'
+      r'\U0001F1E0-\U0001F1FF'
+      r'\u2600-\u27BF'
+      r'\u2700-\u27BF'
+      r'\uFE00-\uFEFF]',
+    message.content
+))
     
     total_emoji = custom_emoji + unicode_emoji
     words = message.content.split()
